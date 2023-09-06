7c50833effff4b2e2c868044a0c958cb0efb8628
fali-aslase <fali@aslase.com>
2023-09-06 16:54:36 +0500

    by ali

---
+++ Javscript/functions.js
@@ -0,0 +1,21 @@
+
+console.log("Learning With Us");
+
+const calculate = (a, b) => {
+    const sum = a + b;
+    const difference = a - b;
+    return sum * difference;
+  };
+  console.log(calculate(8, 3)); 
+
+
+const learn = () => {
+    console.log('Hello, world!');
+    console.log('You Are Learning With Us');
+  };
+learn();
+
+
+const add = (a, b) => a + b;
+
+console.log(add(5, 7));
