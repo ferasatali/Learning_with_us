b39426dbce59f80fc63f628c76d00235c31a5382
ferasatali <ferasatali14@gmail.com>
2023-11-30 11:42:13 -0800

    Fix array sorting logic and add comments

--- Javscript/example.js
+++ Javscript/example.js
@@ -1,11 +1,28 @@
 let x = ["cat", "dog", "bird"];
 
-let y = x.reverse().map(item => item.split('').reverse().join(''));
+let y = x.reverse().map((item) => item.split("").reverse().join(""));
 
-console.log(y, 'y');
-x = [];
+// console.log(y, "y");
 
-console.log(x, 'x');
+let number = [0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0];
+console.log(number.length,"number");
+let k = number.length - 1;
 
-let u = 'abc';
-console.log(u.split(''));
+for (let i = 0; i < number.length; ) {
+  if (number[i] === 0) {
+    i++;
+  } else if (number[i] === 1) {
+    if (number[k] === 0) {
+      number[k] = 1;
+      number[i] = 0;
+      k--;
+      i++;
+      console.log(i,k, 'i,k');
+    }
+    else {
+        k--
+    }
+  }
+}
+
+console.log(number, "number");
